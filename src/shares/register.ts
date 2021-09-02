ea50e387f3b3bc179215b18ead65887016766610
Martin Oppitz <github@martinoppitz.com>
2021-09-02 05:10:54 +0200

    chore: fix code

--- src/shares/register.ts
+++ src/shares/register.ts
@@ -1,31 +1,31 @@
-// import { Configuration, ProjectApi, TimelinesApi } from '@dipa-projekt/dipa-openapi';
-import { DI } from '@leanup/lib';
+import { Configuration, ProjectApi, TimelinesApi } from '@dipa-projekt/dipa-openapi';
+import { DI } from '@leanup/lib/helpers/injector';
 
 import { OIDCService } from '../services/oidc/service';
 
 const oidcService = new OIDCService();
 
-// const API_CONFIG = new Configuration({
-//   basePath: 'http://localhost:8080/api/v1',
-//   accessToken: (): string => {
-//     return oidcService.getUser()?.access_token || '';
-//   },
-// });
+const API_CONFIG = new Configuration({
+  basePath: 'http://localhost:8080/api/v1',
+  accessToken: (): string => {
+    return oidcService.getUser()?.access_token || '';
+  },
+});
 
 DI.register('OIDCService', oidcService);
-// DI.register('TimelinesApi', new TimelinesApi(API_CONFIG));
-// DI.register('ProjectApi', new ProjectApi(API_CONFIG));
+DI.register('TimelinesApi', new TimelinesApi(API_CONFIG));
+DI.register('ProjectApi', new ProjectApi(API_CONFIG));
 
-// setTimeout(() => {
-//   console.log(oidcService.getUser());
-//   DI.get<TimelinesApi>('TimelinesApi')
-//     .getTimelines()
-//     .subscribe({
-//       next: (data) => {
-//         console.log(data);
-//       },
-//       error: (error) => {
-//         console.warn(error);
-//       },
-//     });
-// }, 2000);
+setTimeout(() => {
+  console.log(oidcService.getUser());
+  DI.get<TimelinesApi>('TimelinesApi')
+    .getTimelines()
+    .subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (error) => {
+        console.warn(error);
+      },
+    });
+}, 2000);
