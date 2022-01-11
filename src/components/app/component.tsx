ba6dba7ec3e46b573e8373daa606163c3ce86388
Martin Oppitz <github@martinoppitz.com>
2022-01-11 02:27:25 +0100

    layouting

--- src/components/app/component.tsx
+++ src/components/app/component.tsx
@@ -6,36 +6,59 @@ import { DashboardComponent } from '../dashboard/component';
 
 export const AppComponent: Component<JSX.HTMLAttributes<{}>> = () => {
   return (
-    <div class="container font-sans">
-      <KolNav
-        class="block w-auto p-2"
-        _ariaLabel=""
-        _links={[
-          {
-            _label: 'Test',
-            _icon: 'home',
-          },
-          { _label: 'Test', _icon: 'inbox', _active: true },
-        ]}
-        _orientation="horizontal"
-      />
-      <KolBreadcrumb
-        class="block text-sm mx-2 px-1 border-0 border-b-1"
-        _ariaLabel=""
-        _links={[
-          {
-            _label: 'Startseite',
-          },
-        ]}
-      />
-      <div class="grid grid-cols-2">
-        <aside></aside>
-        <main>
+    <div class="container font-sans grid gap-2">
+      <div class="grid grid-cols-2 bg-#326cae px-1 justify-center item-center ">
+        <KolNav
+          _ariaLabel=""
+          _links={[
+            {
+              _label: 'Test',
+              _icon: 'home',
+            },
+            { _label: 'Test', _icon: 'inbox', _active: true },
+          ]}
+          _orientation="horizontal"
+        />
+        <div class="text-xs py-1.5      ">
+          <KolNav
+            class="inline-block float-right"
+            _ariaLabel=""
+            _links={[
+              {
+                _label: 'Hilfe',
+                _iconOnly: true,
+                _icon: 'question',
+              },
+              {
+                _label: 'Abmelden',
+                _icon: 'logout',
+                _iconOnly: true,
+              },
+            ]}
+            _orientation="horizontal"
+          />
+        </div>
+      </div>
+      <div class="px-0.5 border-0 border-b-1 border-gray-400">
+        <KolBreadcrumb
+          class="block text-sm px-1"
+          _ariaLabel=""
+          _links={[
+            {
+              _label: 'Startseite',
+            },
+          ]}
+        />
+      </div>
+      <div class="grid grid-cols-[auto_2fr] border">
+        <aside class="bg-#326cae"></aside>
+        <main class="px-1">
           <Routes>
             <Route path="/" element={<DashboardComponent />} />
           </Routes>
         </main>
       </div>
+      <div class="fixed bottom-0 p-1 text-center w-100% border-0 border-t-1 border-gray-400">Copyright 2022</div>
     </div>
   );
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.scss';
 import 'uno.css';
+import '@kolibri/lib/www/assets/themes/bmf.css';
 import '@kolibri/lib/www/assets/themes/mapz.css';
 import './shares/constant';
 import './shares/register';
 import './solid.main';
-
